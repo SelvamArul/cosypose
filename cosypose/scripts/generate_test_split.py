434cc149b64800c34d7cc230f9a170edc235efcd
SelvamArul <finditarul@gmail.com>
2021-02-23 13:13:51 +0100

    gen test splits script

---
+++ cosypose/scripts/generate_test_split.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import shutil
+
+def move_split(train_split, test_split):
+    for i in range(60):
+        print (str(train_split / f'{i + 240:06d}'), '-->'  ,str(test_split / f'{i:06d}'))
+        shutil.move(train_split / f'{i + 240:06d}', test_split / f'{i:06d}')
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('dataset_path', type=str, help='an integer for the accumulator')
+    args = parser.parse_args()
+    DATASET_PATH = Path(args.dataset_path)
+    sets = ['pick', 'move']
+
+    for s in sets:
+        move_split(DATASET_PATH / f'train_{s}',  DATASET_PATH / f'test_{s}')
